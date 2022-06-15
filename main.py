e5acc50eb68c2b0ab7cd3e207266987f895acfd5
Hanzalah Rezwan <84363554+HanzalahR@users.noreply.github.com>
2022-06-15 21:52:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+
+window = tk.Tk()
+window.title("Bindi App")  #Change later
+window.geometry("500x500")
+window.mainloop()
+hello = tk.Label(text="Hello world!")
+hello.pack()
+button = tk.Button(text="Click me!")
+button.pack()
+
+tk.mainloop()
+
+
+
+
+
+
+
+
+
+
+
+
+
