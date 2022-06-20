f18a7efa4e8f8f4924ce6de0f9ffe8d4bc82b928
Hanzalah Rezwan <84363554+HanzalahR@users.noreply.github.com>
2022-06-20 01:20:01 +0000

    coming back to this if an error occurs

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 
 window = tk.Tk()
-window.title("Bindi App")  #Change later
+window.title("Climate Change App")  #Change later
 window.geometry("500x500")
 window.mainloop()
 hello = tk.Label(text="Hello world!")
@@ -11,7 +11,24 @@ button.pack()
 
 tk.mainloop()
 
+class Appstarter:
+   def __init__(self, parent):
+        background_color = "Black" # The background colour
+        primary_color = "#FEE715" #font colour for the starting page
+        self.app_frame = Frame(parent, bg = background_color, padx = 100, pady = 100)
+        self.app_frame.grid()
 
+        self.heading_label = Label(self.app_frame, text = "Health and Fitness quiz", font = ("Myriad", "20", "italic", "bold"), fg = primary_color, bg=background_color)
+        self.heading_label.grid(row = 0, padx = 20)
+
+        self.user_label = Label(self.app_frame, text = "Enter username please:", font = ("Myriad", "16"), fg = primary_color, bg = background_color)
+        self.user_label.grid(row = 1, padx = 20, pady = 20)
+
+        self.entry_box = Entry(self.app_frame)
+        self.entry_box.grid(row = 2, padx = 20, pady = 20)
+
+        self.continue_button = Button(self.app_frame, text = "Start", font = ("Myriad", "13"), bg = primary_color, command=self.name_collection)
+        self.continue_button.grid(row = 3, padx = 20, pady = 10)
 
 
 
